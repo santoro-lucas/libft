8a6ff98bc72e9f5aa2fffe10adf331c075fa3dd2
Lucas Santoro <95369010+santoro-lucas@users.noreply.github.com>
2022-04-28 20:12:04 -0300

    First draft of 'ft_strdup.c'

--- libc_functions/ft_strdup.c
+++ libc_functions/ft_strdup.c
@@ -1,1 +1,26 @@
 #include <stdlib.h>
+
+int ft_strlen(char *str)
+{
+    int len;
+    
+    while(*str != '\0')
+    {
+        str++;
+        len++;
+    }
+    return (len);
+}
+
+char    *ft_strdup(char *s)
+{
+    char    *dup;
+    int     len;
+    
+    len = ft_strlen(s);
+    dup = malloc(len * sizeof(char));
+    while (*s != '\0')
+        *dup++ = *s++;
+    dup = dup - len;
+    return (dup);
+}
