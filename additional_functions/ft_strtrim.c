30715a3a3cb4f1dd4d44044a54d88552705bb8eb
Lucas Santoro <95369010+santoro-lucas@users.noreply.github.com>
2022-05-04 21:02:33 -0300

    first draft of ft_strtrim.c

--- additional_functions/ft_strtrim.c
+++ additional_functions/ft_strtrim.c
@@ -1,3 +1,57 @@
+
 #include <stdlib.h>
+// Checks whether char c is in string set
+int	is_in_set(char	c, char *set)
+{
+	while(*set != '\0')
+	{
+		if(c == *set)
+			return (1);
+		else
+			set++;
+	}
+	return (0);
+}
+
+// Returns the lenght of str
+int	ft_strlen(char const *str)
+{
+	int	i;
+
+	i = 0;
+	while (*str++ !='\0')
+		i++;
+	return (i);
+}
 
 char	*ft_strtrim(char const *s1, char const *set)
+{
+	char	*s2;
+
+	s2 = malloc(ft_strlen(s1) * sizeof(char));
+	while(*s1 != '\0')
+	{
+		if(is_in_set)
+			s1++;
+		else
+		{
+			*s2 = *s1;
+			s1++;
+			s2++;
+		}
+	}
+	s2 = s2 - ft_strlen(
+	return (s2);
+}
+
+#include <stdio.h>
+int	main(void)
+{
+	char 	string[] = "ATem dias que a noite é foda ";
+	char	to_trim[] = "A";
+
+	printf("%s\n", string);
+	printf("%s\n", ft_strtrim("ATem dias que a noite é foda", "A"));
+	//free(ft_strtrim);
+	return (0);
+}
