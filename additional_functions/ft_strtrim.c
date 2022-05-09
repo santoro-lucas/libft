44cbf0d63d0cc67719338c07291d2240c60c83e6
Lucas Santoro <95369010+santoro-lucas@users.noreply.github.com>
2022-05-09 20:13:26 -0300

    Makes ft_is* functions. ft_strtrim is WIP

--- additional_functions/ft_strtrim.c
+++ additional_functions/ft_strtrim.c
@@ -1,56 +1,57 @@
 
 #include <stdlib.h>
-// Checks whether char c is in string set
-int	is_in_set(char	c, char *set)
+
+// Gets the lenght of char *str
+int ft_strlen(const char *str)
+{
+	int len;
+	
+	len = 0;
+	while (*str++ != '\0')
+		len++;
+	return (len);
+}
+
+// Checks whether c is present in *set
+int ft_is_in_set(char c, const char *set)
 {
 	while(*set != '\0')
 	{
-		if(c == *set)
-			return (1);
+		if (c == *set)
+			return  (1);
 		else
 			set++;
 	}
 	return (0);
 }
 
-// Returns the lenght of str
-int	ft_strlen(char const *str)
+// To date, creates a duplicate of s1 without chars in *set
+// Must do this only in begining and end of string
+char *ft_strtrim(const char *s1, const char *set)
 {
-	int	i;
-
+	char    *s2;
+	int     i;
+	
 	i = 0;
-	while (*str++ !='\0')
-		i++;
-	return (i);
-}
-
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*s2;
-
 	s2 = malloc(ft_strlen(s1) * sizeof(char));
-	while(*s1 != '\0')
+	while (*s1 != '\0')
 	{
-		if(is_in_set)
-			s1++;
-		else
+		if(!ft_is_in_set(*s1, set))
 		{
 			*s2 = *s1;
-			s1++;
 			s2++;
 		}
+		s1++;
 	}
-	s2 = s2 - ft_strlen(s2);
+	return (s2 - 5);
 }
 
-#include <stdio.h>
-int	main(void)
+// Tests created functions and compares them with some of the originals
+int main()
 {
-	char 	string[] = "ATem dias que a noite é foda ";
-	char	to_trim[] = "A";
-
-	printf("%s\n", string);
-	printf("%s\n", ft_strtrim("ATem dias que a noite é foda", "A"));
-	//free(ft_strtrim);
+	printf("%lu\n", strlen("Hello"));
+	printf("%i\n", ft_strlen("Hello"));
+	printf("%s\n", "abaHello");
+	printf("%s\n", ft_strtrim("abaHebllo", "ab"));
 	return (0);
 }
---
+++ libc_functions/ft_isalpha.c
@@ -0,0 +1,7 @@
+
+int	ft_isalpha(int c)
+{
+	if (c >= 'A' && c <= 'Z' || c >= 'a' && c <='z')
+		return (1);
+	return (0);
+}
