9b83561b0cd14f9ed57a582879d9af01dd4c9959
Lucas Santoro <santoro89@gmail.com>
2022-05-13 23:13:35 +0000

    Delivers the first draft of ft_itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,13 +1,44 @@
 #include <stdlib.h>
 
-char	*ft_itoa(int n)
+#include <unistd.h>
+
+int	count_chars(int n)
 {
-	char *result;
+	unsigned int	figures;
 
-	result = malloc(sizeof(char) * alguem);
+	figures = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		figures++;
+	}
+	while (n > 0)
+	{
+		n /= 10;
+		figures++;
+	}
+	return (figures);
+}
 
-	if (n < 10)
-		*result = n;
-	if (n > 10)
-		ft_itoa(n/10);
+// Code highly reused from ft_putnbr
+// Remember to check
+void	ft_putnbr(int n)
+{
+	char *numstr;
+
+	numstr = malloc((count_chars(n) * sizeof(char)) + 1);
+	if (n < 0)
+	{
+		write(1, "-", 1);
+		n = n * -1;
+	}
+	if (n / 10 < 1)
+		d = '0' + d;
+		write(&d, 1);
+		numstr++;
+	else
+	{
+		ft_putnbr(n / 10);
+		ft_putnbr(n % 10);
+	}
 }
