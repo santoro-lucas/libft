de85d3c7a66c8a13da93309d0307335e61408c81
Lucas Santoro <santoro89@gmail.com>
2022-05-23 18:39:36 +0000

    Updates ft_strtrim draft

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,19 +1,11 @@
 
 #include <stdlib.h>
 
-// Gets the lenght of char *str
-int ft_strlen(const char *str)
-{
-	int len;
-	
-	len = 0;
-	while (*str++ != '\0')
-		len++;
-	return (len);
-}
+// Invokes ft_strlen
+int	ft_strlen(const char *str);
 
 // Checks whether c is present in *set
-int ft_isinset(char c, const char *set)
+static int ft_isinset(char c, const char *set)
 {
 	while(*set != '\0')
 	{
@@ -34,26 +26,33 @@ char *ft_strtrim(const char *s1, const char *set)
 	
 	i = 0;
 	s2 = malloc(ft_strlen(s1) * sizeof(char) + 1);
+	while(ft_isinset(*s1, set))
+		s1++;
 	while (*s1 != '\0')
 	{
-		if(!ft_isinset(*s1, set))
-		{
-			*s2 = *s1;
-			s2++;
-			i++;
-		}
+		*s2 = *s1;
 		s1++;
+		s2++;
+		i++;
 	}
-	*s2 = '\0';
+	s1--;
+	s2--;
+	i--;
+	while(ft_isinset(*s1, set))
+	{
+		*s2 = '\0';
+		s1--;
+		s2--;
+		i--;
+	}
 	return (s2 - i);
 }
 
 // Tests created functions and compares them with some of the originals
+#include <stdio.h>
 int main()
 {
-	printf("%lu\n", strlen("Hello"));
-	printf("%i\n", ft_strlen("Hello"));
-	printf("%s\n", "abaHello");
-	printf("%s\n", ft_strtrim("abaHebllo", "ab"));
+	printf("%s\n", "++--+-+//*Hel++lo**/*+++-");
+	printf("%s\n", ft_strtrim("++--+-+//*Hel+*+lo**/*++*+-", "+-*/"));
 	return (0);
 }
