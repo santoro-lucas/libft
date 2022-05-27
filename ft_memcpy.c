35e13f6268f51ed4e38bf464e29bcdbaca2fe40a
Lucas Santoro <santoro89@gmail.com>
2022-05-27 23:26:03 +0000

    Clears ft_memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/27 21:37:57 by lusantor          #+#    #+#             */
-/*   Updated: 2022/05/27 22:47:36 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/05/27 23:25:25 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,4 @@ void	*ft_memcpy(void *restrict dest, const void *restrict src, int n) // int n s
 		*((char*) dest++) = *((char*) src++);
 	dest -= n;
 	return (dest);
-}
-
-#include <stdio.h>
-void	print_array(int *dest)
-{
-	int	i;
-
-	i = 0;
-	while (i < 5)
-		printf("%i ", dest[i++]);
-	printf("\n");
-}
-
-int	main(void)
-{
-	int	dest[5] = {0,0,0,0,0};
-	int	src[5] = {2147483647,2,3,4,5};
-
-	print_array(dest);
-	ft_memcpy(dest, src, 55);
-	print_array(dest);
-	return (0);
 }
