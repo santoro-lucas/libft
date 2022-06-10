5108baf016cafa3de7880444e34192653bf542a5
Lucas Santoro <santoro89@gmail.com>
2022-06-10 21:20:29 +0000

    Reordered prototypes by recomended order

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:30:39 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/09 18:06:33 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/10 21:19:28 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,39 @@
 # include <stdlib.h>
 # include <unistd.h>
 
-int	ft_atoi(const char *nptr);
-void	ft_bzero(void *s, size_t n);
-// ft_calloc
-int	ft_isalnum(int c);
 int	ft_isalpha(int c);
-int	ft_isascii(int c);
 int	ft_isdigit(int c);
+int	ft_isalnum(int c);
+int	ft_isascii(int c);
 int	ft_isprint(int c);
-// ft_itoa
-// ft_memchr
-// ft_memcmp
+size_t	ft_strlen(const char *str);
+// ft_memset
+void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 // ft_memmove
-// ft_memset
+// ft_strlcat
+// ft_strlcpy
+int	ft_tolower(int c);
+int	ft_toupper(int c);
+// 	ft_strchr
+// 	ft_strrchr
+// 	ft_strncmp
+// 	ft_memchr
+// 	ft_memcmp
+// 	ft_strnstr
+// 	ft_calloc
+char	*ft_strdup(char *s);
+// 		ft_itoa
 void	ft_putchar_fd(char c, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
-char	**ft_split(char const *s, char c);
-// ft_strchr
-char	*ft_strdup(char *s);
-void	ft_striteri(char *s, void (*f)(unsigned int, char*));
+// 		ft_split
+// 		striteri
 char	*ft_strjoin(char const *s1, char const *s2);
-// ft_strlcat
-// ft_strlcpy
-size_t ft_strlen(const char *str);
-char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
-// ft_strncmp
-// ft_strnstr
-// ft_strrchr
-char *ft_strtrim(const char *s1, const  char *set);
-// ft_substr
-int	ft_tolower(int c);
-int	ft_toupper(int c);
+// 		ft_strmapi
+int	ft_atoi(const char *nptr);
+char	*ft_strtrim(const char *s1, const  char *set);
+// 		ft_substr
 
 #endif
