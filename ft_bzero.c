a42125957975174ede3042c537c4a3e2b7daaf20
Lucas Santoro <santoro89@gmail.com>
2022-06-10 22:11:45 +0000

    Delivers memset, refactors bzero and updates libft accordingly

--- ft_bzero.c
+++ ft_bzero.c
@@ -14,14 +14,5 @@
 
 void	ft_bzero(void *s, size_t n)
 {
-	size_t	i;
-
-	i = 0;
-	while (i <= n)
-	{
-		*(char *)s = '\0';
-		s++;
-		i++;
-	}
+	ft_memset(s, '\0', n);
 }
-
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:30:39 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/10 21:19:28 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/10 22:01:45 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
 size_t	ft_strlen(const char *str);
-// ft_memset
+void	ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *restrict dest, const void *restrict src, size_t n);
 // ft_memmove
@@ -35,19 +35,20 @@ int	ft_toupper(int c);
 // 	ft_memchr
 // 	ft_memcmp
 // 	ft_strnstr
+int	ft_atoi(const char *nptr);
 // 	ft_calloc
 char	*ft_strdup(char *s);
+
+// 		ft_substr
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strtrim(const char *s1, const  char *set);
+// 		ft_split
+// 		ft_strmapi
 // 		ft_itoa
+// 		striteri
 void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
-// 		ft_split
-// 		striteri
-char	*ft_strjoin(char const *s1, char const *s2);
-// 		ft_strmapi
-int	ft_atoi(const char *nptr);
-char	*ft_strtrim(const char *s1, const  char *set);
-// 		ft_substr
 
 #endif
