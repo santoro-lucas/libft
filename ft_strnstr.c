f430513246278c50735d8df757e0bb3fb972ac0c
Lucas Santoro <santoro89@gmail.com>
2022-06-19 23:23:22 -0300

    Delivers strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -12,37 +12,6 @@
 
 #include "libft.h"
 
-// char *find_it()
-// {
-
-// }
-
-// char *check_it()
-// {
-// 	while (*(big + buff) == *(little + buff))
-// 		if (*(little + buff) == '\0')
-// 			buff = 0;
-// 			return (big);
-// 		buff++;
-// }
-
-// char	*ft_strnstr(const char	*big, const char *little, size_t len)
-// {
-// 	while(*big != *little)
-// 	{
-// 		big++;
-// 		if (*big == '\0')
-// 			return (NULL);
-// 	}
-// 	while (*(big + buff) == *(little + buff))
-// 	{
-// 		if (*(little + buff) == '\0')
-// 			return (big);
-// 		if (*(big + buff) !)
-// 		buff++;
-// 	}
-// }
-
 char	*ft_strnstr(const char	*big, const char *little, size_t len)
 {
 	int	buff;
@@ -54,11 +23,11 @@ char	*ft_strnstr(const char	*big, const char *little, size_t len)
 			buff++;
 		else if (*big != *(little + buff))
 			buff=0;
+		big++;
 		if (*(little + buff) == '\0')
-			return ((char *) (big - (buff - 1)));
+			return ((char *) (big - (buff)));
 		if (*big == '\0')
 			break;
-		big++;
 	}
 	return (NULL);
 }
