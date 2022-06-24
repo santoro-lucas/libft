e5dc5220a694f6ffa4cbf230841995d19ed2b700
Lucas Santoro <santoro89@gmail.com>
2022-06-24 00:01:01 +0000

    Iterates strnstr

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,31 +6,27 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:38:51 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/23 23:52:37 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/23 23:59:51 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
-#include <string.h>
-#include <bsd/string.h>
-#include <sys/cdefs.h>
 
 char	*ft_strnstr(const char	*big, const char *little, size_t len)
 {
-	size_t	buff;
+	size_t	offset;
 
-	buff = 0;
-	while (len-- && *big != '\0' && *(little + buff) != '\0')
+	offset = 0;
+	while (len-- && *big != '\0' && *(little + offset) != '\0')
 	{
-		if (*big == *(little + buff))
-			buff++;
+		if (*big == *(little + offset))
+			offset++;
 		else
-			buff=0;
+			offset = 0;
 		big++;
 	}
-	if (buff < ft_strlen(little) /*|| *(little + buff) == '\0'*/)
+	if (offset < ft_strlen(little) /*|| *(little + offset) == '\0'*/)
 		return (NULL);
 	else
-		return ((char *) (big - (buff)));
+		return ((char *) (big - (offset)));
 }
