d679c7a617411649b79a74747cd49aef56a34756
Lucas Santoro <santoro89@gmail.com>
2022-06-28 20:28:24 +0000

    Delivers ft_strtrim.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -13,27 +13,42 @@
 #include "libft.h"
 
 // Checks whether c is present in *set
-static int	ft_isinset(char c, const char *set)
-{
-	while (*set != '\0')
-	{
-		if (c == *set)
-			return (1);
-		else
-			set++;
-	}
-	return (0);
-}
+//static int	ft_isinset(char c, const char *set)
+//{
+//	while (*set != '\0')
+//	{
+//		if (c == *set)
+//			return (1);
+//		else
+//			set++;
+//	}
+//	return (0);
+//}
+
+//char	*ft_strtrim(const char *s1, const char *set)
+//{
+//	char	*s2;
+//	int		end;
+//
+//	while (ft_isinset(*s1, set))
+//		s1++;
+//	end = ft_strlen(s1) - 1;
+//	while (ft_isinset(s1[end], set))
+//		end--;
+//	s2 = malloc(end + 2);
+//	ft_strlcpy(s2, s1, end + 2);
+//	return (s2);
+//}
 
 char	*ft_strtrim(const char *s1, const char *set)
 {
 	char	*s2;
-	int		end;
+	size_t	end;
 
-	while (ft_isinset(*s1, set))
+	while (ft_strchr(set, *s1))
 		s1++;
-	end = ft_strlen(s1) - 1;
-	while (ft_isinset(s1[end], set))
+	end = ft_strlen(s1);
+	while (ft_strchr(set, *(s1 + end)))
 		end--;
 	s2 = malloc(end + 2);
 	ft_strlcpy(s2, s1, end + 2);
