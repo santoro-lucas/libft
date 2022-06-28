8b486a530da2b488b7dad5b98abb3ab45c294b78
Lucas Santoro <santoro89@gmail.com>
2022-06-28 23:17:45 +0000

    Delivers final strtrim

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,57 +6,23 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:39:02 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/28 22:54:07 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/28 23:16:46 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// Checks whether c is present in *set
-//static int	ft_isinset(char c, const char *set)
-//{
-//	while (*set != '\0')
-//	{
-//		if (c == *set)
-//			return (1);
-//		else
-//			set++;
-//	}
-//	return (0);
-//}
-
-//char	*ft_strtrim(const char *s1, const char *set)
-//{
-//	char	*s2;
-//	int		end;
-//
-//	while (ft_isinset(*s1, set))
-//		s1++;
-//	end = ft_strlen(s1) - 1;
-//	while (ft_isinset(s1[end], set))
-//		end--;
-//	s2 = malloc(end + 2);
-//	ft_strlcpy(s2, s1, end + 2);
-//	return (s2);
-//}
-
 char	*ft_strtrim(const char *s1, const char *set)
 {
 	char	*s2;
-	size_t	end;
+	size_t	len;
 
 	while (ft_strchr(set, *s1) && *s1 != '\0')
 		s1++;
-
-	end = ft_strlen(s1);
-	if (end)
-	{
-		end--;
-		while (ft_strchr(set, *(s1 + end)))
-			end--;
-		end++;
-	}
-	s2 = malloc(end + 1);
-	ft_strlcpy(s2, s1, end + 1);
+	len = ft_strlen(s1);
+	while (ft_strchr(set, *(s1 + len - 1)) && len > 0)
+		len--;
+	s2 = malloc(len + 1);
+	ft_strlcpy(s2, s1, len + 1);
 	return (s2);
 }
