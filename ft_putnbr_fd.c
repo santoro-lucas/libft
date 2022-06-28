30131b6baaeb57ace38b04cdb359ea8614b3152a
Lucas Santoro <santoro89@gmail.com>
2022-06-28 20:04:04 +0000

    Corrections on putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:36:09 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/27 02:20:49 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/28 19:40:35 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ void	ft_putnbr_fd(int n, int fd)
 	n2 = n;
 	if (n2 < 0)
 	{
-		ft_putchar_fd('-', 1);
+		ft_putchar_fd('-', fd);
 		n2 = n2 * -1;
 	}
 	if (n2 / 10 < 1)
