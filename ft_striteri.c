93dcf6ce39ad7e1d44cc379704c1a3666405b038
Lucas Santoro <santoro89@gmail.com>
2022-06-29 18:19:07 +0000

    Delivers final striteri

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:37:17 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/29 17:46:30 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/29 18:16:07 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 void	ft_striteri(char *s, void (*f)(unsigned int, char*))
 {
-	return (f(s));
+	int	iteri;
+
+	iteri = 0;
+	while (*(s + iteri) != '\0')
+	{
+		f(iteri, (s + iteri));
+		iteri++;
+	}
 }
