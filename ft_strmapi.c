c729e6d690e4ac3eb00e5956772be668f5f8e6e7
Lucas Santoro <santoro89@gmail.com>
2022-06-29 03:32:01 +0000

    Delivers strmapi

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,11 +6,33 @@
 /*   By: lusantor <lusantor@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/09 17:38:18 by lusantor          #+#    #+#             */
-/*   Updated: 2022/06/11 00:06:27 by lusantor         ###   ########.fr       */
+/*   Updated: 2022/06/29 03:31:39 by lusantor         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
-	
+	char	*mapi;
+	int		i;
+
+	i = 0;
+	mapi = malloc(ft_strlen(s) + 1);
+	while (*(s + i) != '\0')
+	{
+		*(mapi + i) = f(i, *(s + i));
+		i++;
+	}
+	return (mapi);
 }
+
+// #include <stdio.h>
+// int main(void)
+// {
+// 	char *result;
+
+// 	result = ft_strmapi("abracadabra", ft_toupper);
+// 	printf("%s\n", result);
+// 	return (0);
+// }
