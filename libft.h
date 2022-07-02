67d4cb7f156210e1bee97be126de86fd1a1a272c
lusantor <lusantor@student.42.rio>
2022-07-02 20:02:03 -0300

    Enforces The Norm in libft.h

--- libft.h
+++ libft.h
@@ -40,7 +40,7 @@ void	*ft_calloc(size_t nmemb, size_t size);
 char	*ft_strdup(char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strtrim(const char *s1, const  char *set);
+char	*ft_strtrim(const char *s1, const char *set);
 char	**ft_split(char const *s, char c);
 char	*ft_itoa(int n);
 char	*ft_strmapi(char const *s, char (*f) (unsigned int, char));
