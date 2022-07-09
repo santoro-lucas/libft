d58f339f94179612c497f0b8e6ec917a42019dad
Lucas Santoro <santoro89@gmail.com>
2022-07-08 23:26:36 -0300

    Delivers lstnew draft

--- Makefile
+++ Makefile
@@ -34,7 +34,8 @@ SRC	=	ft_isalpha.c \
 			ft_putchar_fd.c \
 			ft_putstr_fd.c \
 			ft_putendl_fd.c \
-			ft_putnbr_fd.c
+			ft_putnbr_fd.c \
+			ft_lstnew.c
 
 OBJ = $(SRC:.c=.o) 
 
---
+++ main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "libft.h"
+
+int	main(void)
+{
+	t_list	first;
+	t_list	second;
+	t_list	third;
+	char	*cont_one;
+	char	*cont_two;
+	char 	*cont_three;
+
+	cont_one = ft_strdup("This litle list of mine");
+	cont_two = ft_strdup("it's not much, but it's mine");
+	cont_three = ft_strdup("I care for it");
+	first = ft_lstnew(cont_one);
+	second = ft_lstnew(cont_two);
+	third = ft_lstnew(cont_three);
+	printf("%s\n", first->content);
+	printf("%s\n", second->content);
+	printf("%s\n", third->content);
+	return (0);
+}
